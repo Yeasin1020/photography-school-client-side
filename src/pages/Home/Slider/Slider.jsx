6327ea9481fb04d57bb19d17db7b380dcb98860d
Yeasin1st <ysbd111@gmail.com>
2023-06-07 20:14:28 +0600

    slider add

--- src/pages/Home/Slider/Slider.jsx
+++ src/pages/Home/Slider/Slider.jsx
@@ -16,7 +16,17 @@ const Slider = () => {
 	
 	return (
 		<>
-      
+      <Swiper
+        Autoplay={1000}
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper mt-10 mb-10 rounded-lg"
+      >
+        <SwiperSlide><img className='h-[450px] w-full' src="https://i.ibb.co/XLYhbt3/maxresdefault.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='h-[450px] w-full' src="https://i.ibb.co/F5Kgztt/naturephot-ec32e94608f809e.webp" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='h-[450px] w-full' src="https://i.ibb.co/jkdpQQ9/pedroquintela-1500x844.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='h-[450px] w-full' src="https://i.ibb.co/096Gxdz/pexels-andre-furtado-1264210.jpg" alt="" /></SwiperSlide>
+      </Swiper>
     </>
 	);
 };
